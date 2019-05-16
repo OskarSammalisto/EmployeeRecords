fdaa84044ecff4cea83509cd76f2cfe0b81b717b
oskar <oskarornvs@gmail.com>
2019-05-16 13:39:19 +0200

    Employee finished with put, patch, get and delete.

--- src/main/java/com/example/EmployeeRecords/Employee.java
+++ src/main/java/com/example/EmployeeRecords/Employee.java
@@ -4,13 +4,16 @@ import java.io.File;
 
 public class Employee {
 
+    private int id;
     private String name;
     private String profession;
     private int age;
 
+
    // File employee = new File("employeeRecord.txt");
 
-    public Employee(String name, String profession, int age) {
+    public Employee(int id, String name, String profession, int age) {
+        this.id = id;
         this.name = name;
         this.profession = profession;
         this.age = age;
@@ -26,5 +29,21 @@ public class Employee {
 
     public int getAge() {
         return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- src/main/java/com/example/EmployeeRecords/EmployeeController.java
+++ src/main/java/com/example/EmployeeRecords/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.EmployeeRecords;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,13 +19,18 @@ public class EmployeeController {
     private ArrayList<String> employeeList = new ArrayList<>();
 
     @RequestMapping(method =  RequestMethod.POST)
-    public Employee employee(@RequestParam(value = "name", defaultValue = "Name") String name,
-                             @RequestParam(value = "profession", defaultValue = "Profession") String profession,
-                             @RequestParam(value = "age", defaultValue = "100") int age) {
+    public String postEmployee(@RequestParam(value = "id", defaultValue = "-1") int id,
+                               @RequestParam(value = "name", defaultValue = "Name") String name,
+                               @RequestParam(value = "profession", defaultValue = "Profession") String profession,
+                               @RequestParam(value = "age", defaultValue = "100") int age) {
 
+        for(String string : employeeList) {
+            Employee emp = gson.fromJson(string, Employee.class);
+            if(emp.getId() == id) return "Id: " +emp.getId() +". already exists";
+        }
+        if(id < 1) return "Please enter a valid id!";
 
-       // String returnString = name +", " + profession +", " +age +".";
-        Employee employee = new Employee(name, profession, age);
+        Employee employee = new Employee(id, name, profession, age);
 
         String employeeJson = gson.toJson(employee);
         employeeList.add(employeeJson);
@@ -45,25 +47,72 @@ public class EmployeeController {
 //        }
 
 
-        return employee;
+        return employeeJson;
 
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ArrayList<String> string(@RequestParam(value = "name", defaultValue = " ") String name,
+    public ArrayList<String> getEmployeeList(@RequestParam(value = "id", defaultValue = "-1") int id,
+                                    @RequestParam(value = "name", defaultValue = " ") String name,
                                     @RequestParam(value = "profession", defaultValue = " ")String profession,
-                                    @RequestParam(value = "age", defaultValue = "0")int age) {
+                                    @RequestParam(value = "age", defaultValue = "-1")int age) {
         //String employeeListJson = gson.toJson(employeeList);
 
         ArrayList<String> returnList = new ArrayList<>();
         for(String string : employeeList) {
             Employee emp = gson.fromJson(string, Employee.class);
-           if(emp.getName().equalsIgnoreCase(name)){
+           if(emp.getId() == id || emp.getName().equalsIgnoreCase(name) || emp.getProfession().equalsIgnoreCase(profession) || emp.getAge() == age){
+               returnList.add(string);
+           }
+           else if(id == -1 && name.equalsIgnoreCase(" ") && profession.equalsIgnoreCase(" ") && age == -1) {
                returnList.add(string);
            }
         }
 
         return returnList;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public String deleteEmployee(@RequestParam(value = "id", defaultValue = "-1") int id) {
+        for(String string :employeeList) {
+            Employee emp = gson.fromJson(string, Employee.class);
+            if(emp.getId() == id) {
+                String name = emp.getName();
+                employeeList.remove(string);
+                return "Employee " +name +" with the Id: " +id +". deleted!";
+            }
+        }
+
+        return "Id not found!";
+
+    }
+
+
+    @RequestMapping(method = RequestMethod.PATCH)
+    public String patchEmployee(@RequestParam(value = "id", defaultValue = "-1") int id,
+                                @RequestParam(value = "name", defaultValue = " ") String name,
+                                @RequestParam(value = "profession", defaultValue = " ")String profession,
+                                @RequestParam(value = "age", defaultValue = "-1")int age) {
+
+        for(String string :employeeList) {
+            Employee emp = gson.fromJson(string, Employee.class);
+            if(emp.getId() == id) {
+
+                if(!name.equalsIgnoreCase(" ")) emp.setName(name);
+                if(!profession.equalsIgnoreCase(" ")) emp.setProfession(profession);
+                if(age >= 0) emp.setAge(age);
+
+                employeeList.remove(string);
+                employeeList.add(gson.toJson(emp));
+
+
+                return "Employee updated! \r\n" +gson.toJson(emp);
+            }
+        }
+
+
+
+        return "Employee id not found!";
     }
 
 }
